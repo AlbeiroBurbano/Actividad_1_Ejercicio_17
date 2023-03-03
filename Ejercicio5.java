cd10231f36267b79dec7879e058440ecc8e71543
AlbeiroBurbano <albeirojbt@gmail.com>
2023-03-03 01:14:25 -0500

    Albeiro

---
+++ Ejercicio5.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio17;
+
+/**
+ *
+ * @author albei
+ */
+public class Ejercicio5 {
+    public static void main(String[] args) {
+    
+    
+    
+    
+    }
+}
